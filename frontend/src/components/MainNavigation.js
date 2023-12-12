e452f7d1480cf01800d62d1af587c7ab8525bc82
chewho <molocrips@gmail.com>
2023-12-12 16:55:50 +0100

    feat: add auth page, add new auth link

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,7 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
   return (
@@ -14,8 +14,7 @@ function MainNavigation() {
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
-              end
-            >
+              end>
               Home
             </NavLink>
           </li>
@@ -24,8 +23,7 @@ function MainNavigation() {
               to="/events"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
-              }
-            >
+              }>
               Events
             </NavLink>
           </li>
@@ -34,9 +32,17 @@ function MainNavigation() {
               to="/newsletter"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
-              }
-            >
+              }>
               Newsletter
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/auth"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }>
+              Authentication
             </NavLink>
           </li>
         </ul>
